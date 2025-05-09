fb650283573a7116f2fa52f796a69c158d696b0f
Wiftsack420 <mbo8292@students.fhv.at>
2025-05-09 12:45:59 +0200

    initial commit

---
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/environment/WeatherSimulation.java
@@ -0,0 +1,17 @@
+package at.fhv.sysarch.lab2.homeautomation.environment;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import at.fhv.sysarch.lab2.homeautomation.devices.WeatherSensor;
+
+/*
+public class WeatherSimulation extends AbstractBehavior<WeatherSimulation.WeatherSimulationCommand> {
+
+    public interface WeatherSimulationCommand {}
+
+    private static class Tick implements WeatherSimulationCommand {}
+
+
+}
+
+*/
