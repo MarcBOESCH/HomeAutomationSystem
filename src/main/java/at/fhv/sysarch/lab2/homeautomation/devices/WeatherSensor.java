3ce209ba9c29cd93d1c50f0ee7e3b87f46aa8453
Wiftsack420 <mbo8292@students.fhv.at>
2025-05-09 13:04:19 +0200

    initial commit

--- src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/WeatherSensor.java
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/devices/WeatherSensor.java
@@ -1,4 +1,54 @@
 package at.fhv.sysarch.lab2.homeautomation.devices;
 
-public class WeatherSensor {
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+
+public class WeatherSensor extends AbstractBehavior<WeatherSensor.WeatherCommand> {
+
+    public interface WeatherCommand {}
+
+    public static final class ReadWeather implements WeatherCommand {
+        final String condition;
+
+        public ReadWeather(String condition) {
+            this.condition = condition;
+        }
+    }
+
+    // Initializes the Actor
+    public static Behavior<WeatherSensor.WeatherCommand> create(ActorRef<AirCondition.AirConditionCommand> airCondition) {
+        return Behaviors.setup(context -> new WeatherSensor(context));
+    }
+
+    public WeatherSensor(ActorContext<WeatherCommand> context) {
+        super(context);
+
+        getContext().getLog().info("WeatherSensor started");
+    }
+
+    @Override
+    public Receive<WeatherCommand> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(ReadWeather.class, this::onReadWeather)
+                .onSignal(PostStop.class, signal -> onPostStop())
+                .build();
+    }
+
+    // Defines the behavior
+    private Behavior<WeatherSensor.WeatherCommand> onReadWeather(ReadWeather message) {
+        getContext().getLog().info("TemperatureSensor received {}", message.condition);
+
+        return this;
+    }
+
+    private WeatherSensor onPostStop() {
+        getContext().getLog().info("WeatherSensor actor {}-{} stopped");
+        return this;
+    }
 }
